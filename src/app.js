a2767f26fbb2bb2bfc85e89dcffac2ed38de483f
Orlando Gomes <orlandosjm@yahoo.com.br>
2020-03-30 18:37:56 -0300

    Config. avançadas - exceções e variáveis ambiente

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
+import 'dotenv/config';
+
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
+// Sentry is a Exception handler
+import * as Sentry from '@sentry/node';
+import sentryConfig from './config/sentry';
+
+// That's for Sentry handlering asyncronous req/res
+import 'express-async-errors';
 
 import routs from './routes';
 
@@ -9,12 +18,19 @@ class App {
   constructor() {
     this.server = express();
 
+    // Sentry is a Exception handler
+    Sentry.init(sentryConfig);
+
     this.middlewares();
 
     this.routes();
+
+    this.exceptionHandler();
   }
 
   middlewares() {
+    // Sentry is a Exception handler
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -24,6 +40,20 @@ class App {
 
   routes() {
     this.server.use(routs);
+    // Sentry is a Exception handler
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://f5aaf1f4ce6741d39d39fde4f6539a77@sentry.io/5181730',
+};
